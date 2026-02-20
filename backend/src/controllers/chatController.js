6aa2590ddeb5e0c6bd93f2838fa4a5e7fb4ab40f
ASHWANEETH <ashwaneeth@gmail.com>
2026-02-20 13:33:51 +0530

    refactor

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -2,16 +2,15 @@ import { chatClient } from "../lib/stream.js";
 
 export async function getStreamToken(req, res) {
   try {
-    const streamToken = await chatClient.createToken(req.user.clerkId);
+    const streamToken = chatClient.createToken(req.user.clerkId);
     res.status(200).json({
       streamToken,
       userId: req.user.clerkId,
       userName: req.user.name,
       userImage: req.user.profileImage,
-      msg: "Token generated sucessfullly",
     });
   } catch (error) {
-    res.status(500);
-    console.error("Error generating stream token!");
+    res.status(500).json({ msg: "Error generating stream token!" });
+    console.error("Error generating stream token!", error);
   }
 }
