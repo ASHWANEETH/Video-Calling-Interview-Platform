c8649c94d08b37c8db27a6a684d9ded8359eac8b
ASHWANEETH <ashwaneeth@gmail.com>
2026-02-22 21:27:18 +0530

    suggestion_fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const api_url = import.meta.env.VITE_API_URL;
+if (!api_url) {
+  throw new Error("API URL is not defined in environment variables");
+}
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: api_url,
   withCredentials: true,
 });
 
