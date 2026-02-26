5c583d81511740cee63cbc5fe037bf936ebcc065
ASHWANEETH <ashwaneeth@gmail.com>
2026-02-27 00:52:13 +0530

    dash-board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { useUser } from "@clerk/clerk-react";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -11,12 +12,19 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
         />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
       <Toaster />
     </>
